d8e9dc2c3ea60f08ae355f325ed544bd5decb75e
Vampir55 <ncr2005@mail.ru>
2021-01-13 01:44:57 +0500

    New project "Bubbles" Added functions and Class Bubble

--- Bubbles/main.py
+++ Bubbles/main.py
@@ -6,30 +6,31 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
-display = pg.display.set_mode((300, 200))
-display.fill(WHITE)
+screen = pg.display.set_mode((600, 400), 0, 32)
+pg.display.init()
+screen.fill(WHITE)
 pg.display.update()
-pg.time.delay(1000)
-
-class Bubble:
-    x = 0
-    y = 0
-    radius = 1
-    colors = [RED, GREEN, BLUE]
-    color = colors[1]
 
 
-def main(disp):
-    disp.display.update()
-    while 1:
-        for i in disp.event.get():
-            if i.type == disp.QUIT:
-                disp.quit()
+# class Bubble:
+#     x = 0
+#     y = 0
+#     radius = 1
+#     colors = [RED, GREEN, BLUE]
+#     color = colors[1]
+#
+#
+# def main(disp):
+#     disp.display.update()
+#     while 1:
+#         for i in disp.event.get():
+#             if i.type == disp.QUIT:
+#                 disp.quit()
+#
+#
+# def drawbubble(Bubble):
+#     print("drawing bubble", Bubble.x, Bubble.y, Bubble.radius, Bubble.color)
+#     pg.draw.circle(screen, Bubble.color, (Bubble.x, Bubble.y), Bubble.radius)
+#
 
-
-def drawbubble(Bubble):
-    print("drawing bubble", Bubble.x, Bubble.y, Bubble.radius, Bubble.color)
-    pg.draw.circle(display, Bubble.color, (Bubble.x, Bubble.y), Bubble.radius)
-
-
-main(pg)  # call main function
+# main(pg)  # call main function
