56f877c10f6e7c4f32ad2dccf28a5b6b13a298df
Vampir55 <ncr2005@mail.ru>
2021-01-13 01:16:57 +0500

    New project "Bubbles" Added functions and Class Bubble

--- Bubbles/main.py
+++ Bubbles/main.py
@@ -1,24 +1,35 @@
 # My test project "Bubbles" to learning Classes and pygame lib
 import pygame as pg
 
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
 
-display = pg.display.set_mode((1000, 500))
+display = pg.display.set_mode((300, 200))
+display.fill(WHITE)
+pg.display.update()
+pg.time.delay(1000)
 
 class Bubble:
     x = 0
     y = 0
     radius = 1
-    colors = ['RED', 'GREEN', 'BLUE']
+    colors = [RED, GREEN, BLUE]
     color = colors[1]
 
 
-def main():
-    return 0
+def main(disp):
+    disp.display.update()
+    while 1:
+        for i in disp.event.get():
+            if i.type == disp.QUIT:
+                disp.quit()
 
 
 def drawbubble(Bubble):
     print("drawing bubble", Bubble.x, Bubble.y, Bubble.radius, Bubble.color)
-    pg.draw.circle(1, Bubble.color, (Bubble.x, Bubble.y), Bubble.radius)
+    pg.draw.circle(display, Bubble.color, (Bubble.x, Bubble.y), Bubble.radius)
 
 
-main()  # call main function
+main(pg)  # call main function
