4064505d07b173f826836ce02d79ffbdcb51e407
Vampir55 <ncr2005@mail.ru>
2021-01-13 00:46:45 +0500

    New project "Bubbles" Added functions and Class Bubble

--- Bubbles/main.py
+++ Bubbles/main.py
@@ -1,1 +1,19 @@
 # My test project "Bubbles" to learning Classes and pygame lib
+from pygame import draw
+class Bubble:
+    x = 0
+    y = 0
+    radius = 1
+    colors = ['RED', 'GREEN', 'BLUE']
+    color = colors[1]
+
+
+def main():
+    return 0
+
+
+def drawbubble(Bubble):
+    print("drawing bubble", Bubble.x, Bubble.y, Bubble.radius, Bubble.color)
+
+
+main()  # call main function
