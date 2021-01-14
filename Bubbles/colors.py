d908dc4e6fc39df47613e87ae0d21846c310be47
Vampir55 <ncr2005@mail.ru>
2021-01-14 23:36:33 +0500

    New project "Bubbles" Added functions and Class Bubble New file colors

---
+++ Bubbles/colors.py
@@ -0,0 +1,10 @@
+#colors file for my projects
+
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+PURPLE = (255, 0, 255)
+ORANGE = (150, 150, 100)
+BLACK = (0, 0, 0)
+YELLOW = (100, 255, 100)
--- Bubbles/main.py
+++ Bubbles/main.py
@@ -1,10 +1,6 @@
 # My test project "Bubbles" to learning Classes and pygame lib
 import pygame as pg
 
-WHITE = (255, 255, 255)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
 
 screen = pg.display.set_mode((600, 400), 0, 32)
 pg.display.init()
@@ -12,25 +8,31 @@ screen.fill(WHITE)
 pg.display.update()
 
 
-# class Bubble:
-#     x = 0
-#     y = 0
-#     radius = 1
-#     colors = [RED, GREEN, BLUE]
-#     color = colors[1]
-#
-#
-# def main(disp):
-#     disp.display.update()
-#     while 1:
-#         for i in disp.event.get():
-#             if i.type == disp.QUIT:
-#                 disp.quit()
-#
-#
-# def drawbubble(Bubble):
-#     print("drawing bubble", Bubble.x, Bubble.y, Bubble.radius, Bubble.color)
-#     pg.draw.circle(screen, Bubble.color, (Bubble.x, Bubble.y), Bubble.radius)
-#
+class Bubble:
+    x = 0
+    y = 0
+    radius = 1
+    colors = [RED, GREEN, BLUE]
+    color = colors[1]
 
-# main(pg)  # call main function
+disp.display.update()
+ while 1:
+     for i in disp.event.get():
+        if i.type == disp.QUIT:
+            disp.quit()
+
+Bub1 = Bubble()
+Bub1.color = 1
+Bub1.radius = 20
+Bub1.x=100
+Bub1.y=200
+drawbubble(Bub1)
+
+
+
+def drawbubble(Bubble):
+    print("drawing bubble", Bubble.x, Bubble.y, Bubble.radius, Bubble.color)
+    pg.draw.circle(screen, Bubble.color, (Bubble.x, Bubble.y), Bubble.radius)
+
+
+
