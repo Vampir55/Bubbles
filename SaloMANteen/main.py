9eff6a1f78d684d3fda8cc56f832be72fe43e030
Vampir55 <ncr2005@mail.ru>
2021-01-27 00:04:16 +0500

    Add new files Add new classes

---
+++ SaloMANteen/main.py
@@ -0,0 +1,30 @@
+# My new game SaloMANteen
+import sys
+import random as rnd
+import pygame as pg
+
+
+# Add new Classes
+class GameWindow:
+    pass
+
+
+class GameObject:
+    pass
+
+
+class GameScene(GameObject):
+    pass
+
+
+class Player(GameObject):
+    pass
+
+
+class Enemy(GameObject):
+    pass
+
+
+class Block(GameObject):
+    pass
+
