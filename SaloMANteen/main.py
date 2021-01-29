e1df4d86575331db231bc387546b6d70912fb02a
Vampir55 <ncr2005@mail.ru>
2021-01-30 01:50:57 +0500

    Add new files Add new classes Game Window class fill gameObject filled Movement

--- SaloMANteen/main.py
+++ SaloMANteen/main.py
@@ -20,7 +20,7 @@ class GameWindow:
         clock = pg.time.Clock()
         scene = GameScene()
         player = Player()
-        player.y = settings.WIDTH - 20 - settings.player_HEIGHT
+        player.y = settings.WIDTH - 20 - settings.player_WIDTH
         while not self.is_finished:
             # Draw scene
             scene.drawscene()
@@ -29,16 +29,26 @@ class GameWindow:
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     self.is_finished = True
-            if keys[pg.K_RIGHT]:
+            if keys[pg.K_RIGHT] and player.x <= settings.HEIGHT/2-settings.player_HEIGHT:
                 # Right arrow
                 player.x += player.speed
-            elif keys[pg.K_LEFT]:
+            if keys[pg.K_LEFT] and player.x >= 5:
                 # Left arrow
                 player.x -= player.speed
-            elif keys[pg.K_SPACE]:
-                pass  # Space key for jump
+            if not player.is_jump:
+                if keys[pg.K_SPACE]:
+                    # Space key for jump
+                    player.is_jump = True
             else:
-                pass  # Stand don't move
+                if player.jump_count <= 0 and player.jump_count >= -10:
+                    player.y -= (player.jump_count ** 2) / 2
+                elif player.jump_count <= 10:
+                    player.y += (player.jump_count ** 2) / 2
+                player.jump_count += 1
+                if player.jump_count > 10:
+                    player.jump_count = -10
+                    player.is_jump = False
+
             # Draw objects
             pg.display.update()
             clock.tick(settings.FPS)
@@ -60,6 +70,9 @@ class GameScene(GameObject):
 
 
 class Player(GameObject):
+    jump_count = -10
+    is_jump = False
+
     def drawplayer(self):
         pg.draw.rect(GameWindow.screen, (25, 0, 255), (self.x, self.y, settings.player_HEIGHT, settings.player_WIDTH))
 
